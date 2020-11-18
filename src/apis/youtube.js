cca6b3a4406a7383c7cc1c48178697484429565c
pragadeesh2050 <pragadeesh2050@gmail.com>
2020-11-18 21:29:33 +0530

    ignoring youtube.js as of now

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const KEY = '';
+const KEY = 'AIzaSyCzkAbY9OrTTg_GZ3z1dhkOnfcFnzbWRwc';
 
 export default axios.create({
   baseURL: 'https://www.googleapis.com/youtube/v3',
